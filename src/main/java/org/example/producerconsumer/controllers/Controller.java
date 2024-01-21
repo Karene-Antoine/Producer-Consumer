40ad7890b139d85e44a9fc350c60711a0dece5d1
Armaritto <armia.joseph35@gmail.com>
2024-01-21 20:26:37 +0200

    Update Controller.java
    Co-Authored-By: Karene-Antoine <117857702+Karene-Antoine@users.noreply.github.com>

--- src/main/java/org/example/producerconsumer/controllers/Controller.java
+++ src/main/java/org/example/producerconsumer/controllers/Controller.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Queue;
+@CrossOrigin
 @RestController
 public class Controller {
     GraphBuilder buildGraph;
